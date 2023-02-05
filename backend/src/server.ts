d89bbdfc1893aa5abf94f0e8d74dca22b94251d5
LM-Inocentes <louisemartininocentes@gmail.com>
2023-02-05 15:07:32 +0800

    setting up database

---
+++ backend/src/server.ts
@@ -0,0 +1,35 @@
+//package installs
+// npm install express cors
+// npm install ts-node --save-dev
+// npm install nodemon --save-dev
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { dbConnect } from './configs/database.config';
+import express from "express";
+import cors from "cors";
+
+import userRouter from './router/user.router';
+
+
+
+dbConnect();
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin:["http://localhost:4200"],
+}));
+
+app.use("/api/users/", userRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Website is served on http://localhost:" +port);
+})
