ff725d6f12dab150b510b189c4cee21382d422dd
LM-Inocentes <louisemartininocentes@gmail.com>
2023-02-12 21:33:07 +0800

    home header

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { HeaderComponent } from './components/header/header.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 const routes: Routes = [
-  {path : '', title: 'Home', component : LandingPageComponent},
-  {path : 'login',title: 'Login',  component : LoginComponent},
-  {path : 'register',title: 'Register',  component : RegisterComponent},
+  {path : '', title: 'LOOK4ME', component : LandingPageComponent},
+  {path : 'home', title: 'LOOK4ME:Home', component : HeaderComponent},
+  {path : 'login',title: 'LOOK4ME:Login',  component : LoginComponent},
+  {path : 'register',title: 'LOOK4ME:Register',  component : RegisterComponent},
 ];
 
 @NgModule({
