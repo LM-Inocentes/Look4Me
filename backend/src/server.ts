8b1aea2f5fdb2487700cdfcfd7e9f0a1c249d02c
LM-Inocentes <louisemartininocentes@gmail.com>
2023-04-10 19:19:34 +0800

    cors update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,10 +23,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:8888', 'http://127.0.0.1:8888'],
-}));
+app.use(cors());
 
 app.use("/api/users/", userRouter);
 app.use("/api/items/", itemRouter);
