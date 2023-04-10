2f1c89f84bf8b773e1a27f71174fcb0907814e6a
LM-Inocentes <louisemartininocentes@gmail.com>
2023-04-10 19:00:09 +0800

    Revert "Merge pull request #1 from LM-Inocentes/deploy-backend"
    This reverts commit d1b0223271ef6b575ed373f3ce2b71cbbff2c350, reversing
    changes made to db05af1f5b59c7544ef1763e3a3f3519d47ac053.

---
+++ frontend/src/app/components/found-info-page/found-info-page.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ItemsService } from 'src/app/services/items.service';
+import { UserService } from 'src/app/services/user.service';
+import { Item } from 'src/app/shared/models/Item';
+import { User } from 'src/app/shared/models/User';
+
+
+@Component({
+  selector: 'app-found-info-page',
+  templateUrl: './found-info-page.component.html',
+  styleUrls: ['./found-info-page.component.css']
+})
+export class FoundInfoPageComponent {
+  item = {} as Item;
+  user!:User;
+
+  constructor(private activatedRoute:ActivatedRoute, private itemService: ItemsService,
+     private router: Router, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+      console.log(this.user);
+    });
+    this.activatedRoute.params.subscribe((params) => {
+        this.itemService.getItemByID(params.itemID).subscribe(serverItem => {
+        this.item = serverItem;
+      });
+    })
+  }
+  get isPoster(){
+    return (this.item.poster_email===this.user.email)||("admin@gmail.com"===this.user.email);
+  }
+  get isAdmin(){
+    return ("admin@gmail.com"===this.user.email);
+  }
+  get isAuth(){
+    return this.user.token;
+  }
+  isClaimed(){
+    this.itemService.claimPost(this.item.id, this.user).subscribe(_ => {
+      this.router.navigateByUrl('/found-items/info/'+this.item.id);
+      this.ngOnInit();
+    });
+  }
+  get alreadyClaimed(){
+    return this.item.retriever_id;
+  }
+  get accessAdmin(){
+    return ("admin@gmail.com"===this.user.email);
+  }
+  get accessClaim(){
+    return (this.item.retriever_id===this.user.id);
+  }
+
+  get accessOwner(){
+    return (this.item.returned_id===this.user.id);
+  }
+
+  postDelete(){
+    this.itemService.deleteItemByID(this.item.id)
+    .subscribe(_ => {
+      this.router.navigateByUrl('/found-items');
+      this.ngOnInit();
+    });
+  }
+}
